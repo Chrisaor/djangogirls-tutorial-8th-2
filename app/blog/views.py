6670cd9923aa467ed659bde3b1ede500c3f458fe
chrisaor <pchpch0070@gmail.com>
2018-06-06 15:12:08 +0900

    post_list에서 blog/post_list.html파일을 동적으로 렌더링

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from blog.models import Post
+
+
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+    posts = Post.objects.all()
+    context = {
+        'posts':posts,
+    }
+    return render(request, 'blog/post_list.html', context)
